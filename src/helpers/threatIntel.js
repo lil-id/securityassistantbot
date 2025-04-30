b7b57cc3fd040a30488be3f10b4f3a5ec283b792
lil-id <holilhaqalimhakim@gmail.com>
2025-04-30 15:18:34 +0800

    update: fix threat intel checking

--- src/helpers/threatIntel.js
+++ src/helpers/threatIntel.js
@@ -1,5 +1,5 @@
-const { abuseIpDBCheck } = require('./abuseipdb/abuseCheck');
-const { threatFoxCheck } = require('./threatfox/threatFox');
+const { abuseIpDBCheck } = require("./abuseipdb/abuseCheck");
+const { threatFoxCheck } = require("./threatfox/threatFox");
 
 /**
  * Cek IP di ThreatFox & AbuseIPDB
@@ -13,41 +13,48 @@ const { threatFoxCheck } = require('./threatfox/threatFox');
  * }}
  */
 async function checkThreatIntel(ip) {
-  let abuseResult = null;
-  let threatResult = null;
-  let error = false;
-  const sources = [];
+    let abuseResult = null;
+    let threatResult = null;
+    let error = false;
+    const sources = [];
 
-  try {
-    abuseResult = await abuseIpDBCheck(ip);
-    if (abuseResult?.data) {
-      sources.push('AbuseIP DB');
+    try {
+        abuseResult = await abuseIpDBCheck(ip);
+
+        if (
+            !abuseResult ||
+            (abuseResult?.data === null &&
+                abuseResult?.errors?.[0]?.status === 429)
+        ) {
+            error = true;
+        } else if (abuseResult?.data) {
+            sources.push("AbuseIP DB");
+        }
+    } catch (err) {
+        error = true;
     }
-  } catch (err) {
-    error = true;
-  }
 
-  try {
-    threatResult = await threatFoxCheck(ip);
-    if (threatResult) {
-      sources.push('ThreatFox');
+    try {
+        threatResult = await threatFoxCheck(ip);
+        if (threatResult) {
+            sources.push("ThreatFox");
+        }
+    } catch (err) {
+        error = true;
     }
-  } catch (err) {
-    error = true;
-  }
 
-  const confidence =
-    threatResult?.confidence_level ??
-    abuseResult?.data?.abuseConfidenceScore ??
-    null;
+    const confidence =
+        threatResult?.confidence_level ??
+        abuseResult?.data?.abuseConfidenceScore ??
+        null;
 
-  return {
-    confidence,
-    sources,
-    error,
-    abuseRaw: abuseResult,
-    threatFoxRaw: threatResult,
-  };
+    return {
+        confidence,
+        sources,
+        error,
+        abuseRaw: abuseResult,
+        threatFoxRaw: threatResult,
+    };
 }
 
 module.exports = { checkThreatIntel };
